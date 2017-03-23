1515103ff77f4377734ca79e6ca41f5c4b932336
Emma Phiri <emma@uber5.com>
2017-03-23 20:41:40 +0200

    attachments

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ app.post('/webhook/', function (req, res) {
 			receivedMessage(event)
 			let text = event.message.text
 			decideMessage(sender, text)
-		} else if (event.message.attachment) {
+		} else if (event.message.attachments) {
 			receivedMessage(event)
 			let text = event.message.attachments[0].title
 			console.log(text);
