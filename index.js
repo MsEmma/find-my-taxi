96fbfd7b445c78cd00065b9e00ae3832389f1bb7
Emma Phiri <emma@uber5.com>
2017-04-17 19:24:42 +0200

    results from wimt api

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 'use strict'
 
 const express = require('express')
+const session = require('express-session')
 const bodyParser = require('body-parser')
 const request = require('request')
 const app = express()
@@ -30,6 +31,55 @@ app.get('/webhook/', function (req, res) {
 		res.send('Error, wrong token')
 	}
 })
+
+// whereismytransport api call
+function wimtAPICall() {
+
+	return new Promise((resolve, reject) => {
+
+		const CLIENT_ID = '709b10a8-342e-4637-8cd0-47d48107c31d';
+  	const CLIENT_SECRET = 'ShDVjohLxFrsiFxM7tNlVZ8QMnuENDAGjVHKpKNsxgE=';
+
+  	const clientOptions = {
+      method: "POST",
+      headers: "ACCEPT: application/json",
+      url: "https://identity.whereismytransport.com/connect/token",
+      form: {
+          client_id: CLIENT_ID,
+          client_secret: CLIENT_SECRET,
+          grant_type: "client_credentials",
+          scope: "transportapi:all"
+      }
+  	}
+
+  	request(clientOptions, function (error, response, body) {
+
+    	const TOKEN = JSON.parse(body).access_token;
+
+    	var body = {
+      	geometry: {
+        	type: "Multipoint",
+        	coordinates: [[18.425059, -33.922138], [18.399267, -33.908676]]
+      	}
+    	}
+
+	    var options = {
+	      method: "POST",
+	      headers: {
+	        "Accept": "application/json",
+	        "Content-Type": "application/json",
+	        "Authorization": "Bearer " + TOKEN
+	      },
+	      url: "https://platform.whereismytransport.com/api/journeys?exclude=geometry",
+	      body: JSON.stringify(body)
+	    }
+
+	    request(options, function (error, response, body) {
+	      return resolve(JSON.parse(body))
+	    })
+  	})
+	})
+}
 
 // to post data
 app.post('/webhook/', function (req, res) {
@@ -144,31 +194,126 @@ function sendImageMessage(sender, imageURL) {
 	sendRequest(sender, messageData)
 }
 
-function sendGenericMessage(sender) {
-	let messageData = {
-		"attachment": {
-      "type":"template",
-      "payload":{
-        "template_type":"generic",
-        "elements":[
-           {
-            "title":"Seapoint",
-						"image_url":"http://www.capetownlife.co.za/wp-content/uploads/2014/05/Sea-Point.jpg",
-            "subtitle":"I love the sea",
-            "buttons":[
-              {
-                "type":"web_url",
-                "url":"https://en.wikipedia.org/wiki/Sea_Point",
-                "title":"More about Seapoint"
-              }
-            ]
-          }
-        ]
-      }
-    }
-	}
-	sendRequest(sender, messageData)
+app.get('/wimt', function(req, res){
+	journeyDetails()
+	.then(result => {
+		// req.session.details = result
+		res.json(result)
+	})
+})
+
+function journeyDetails() {
+
+	return wimtAPICall()
+	.then(result => {
+		return result.itineraries.map(itinerary => {
+			return itinerary.legs
+		})
+	})
+	.then(legs => {
+		return legs.map(leg => {
+			return leg.map(lp => {
+				if(lp.type === "Transit") {
+					return {
+						mode: "Minibus taxi",
+						distance: `${lp.distance.value}${lp.distance.unit}`,
+						route: lp.line.name,
+						fare: `R${lp.fare.cost.amount}`
+					}
+				} else {
+					return {
+						mode: lp.type,
+						distance: `${lp.distance.value} ${lp.distance.unit}`,
+						directions: lp.directions.map(dir =>{
+							return `${dir.instruction} for ${dir.distance.value}m`
+						})
+					}
+				}
+			})
+		})
+	})
+	.then(l => {
+		// console.log("journeyDetails", l)
+		return l
+	})
 }
+
+// [
+// 	[ { mode: 'Walking', distance: '3376 m', directions: [Object] } ],
+//
+//   [ { mode: 'Walking', distance: '630 m', directions: [Object] },
+//     { mode: 'Minibus taxi',
+//       distance: '10541m',
+//       route: 'Cape Town Taxi Rank to Camps Bay',
+//       fare: 'R9' },
+//     { mode: 'Walking', distance: '86 m', directions: [Object] } ],
+//
+//   [ { mode: 'Walking', distance: '630 m', directions: [Object] },
+//     { mode: 'Minibus taxi',
+//       distance: '10541m',
+//       route: 'Cape Town Taxi Rank to Camps Bay',
+//       fare: 'R9' },
+//     { mode: 'Walking', distance: '86 m', directions: [Object] } ]
+// ]
+
+function sendGenericMessage(sender) {
+
+	journeyDetails()
+	.then(result => {
+
+		let option2 = result[1]
+
+		let messageData = {
+			"attachment": {
+	      "type":"template",
+	      "payload":{
+	        "template_type":"generic",
+	        "elements":[
+						{
+							"title":"First leg",
+							"subtitle": option2[0].mode + "for" +  option2[0].distance
+						},
+						{
+							"title":"Second leg",
+							"subtitle": "Take a " + option2[1].mode + " from " + option2[1].route + " for " +  option2[1].distance
+						},
+						{
+							"title":"Last leg",
+							"subtitle": option2[2].mode + "for" +  option2[2].distance
+						}
+	        ]
+	      }
+	    }
+		}
+		sendRequest(sender, messageData)
+	})
+}
+
+// function sendGenericMessage(sender) {
+// 	let messageData = {
+// 		"attachment": {
+//       "type":"template",
+//       "payload":{
+//         "template_type":"generic",
+//         "elements":[
+//            {
+//             "title":"Seapoint",
+// 						"image_url":"http://www.capetownlife.co.za/wp-content/uploads/2014/05/Sea-Point.jpg",
+//             "subtitle":"I love the sea",
+//             "buttons":[
+//               {
+//                 "type":"web_url",
+//                 "url":"https://en.wikipedia.org/wiki/Sea_Point",
+//                 "title":"More about Seapoint"
+//               }
+//             ]
+//           }
+//         ]
+//       }
+//     }
+// 	}
+// 	sendRequest(sender, messageData)
+// }
 
 function sendLocation(sender) {
 	let messageData = {
@@ -200,58 +345,6 @@ function sendRequest(sender, messageData) {
 		}
 	})
 }
-
-function getJourney() {
-
-	const CLIENT_ID = '709b10a8-342e-4637-8cd0-47d48107c31d';
-  const CLIENT_SECRET = 'ShDVjohLxFrsiFxM7tNlVZ8QMnuENDAGjVHKpKNsxgE=';
-
-  const clientOptions = {
-      method: "POST",
-      headers: "ACCEPT: application/json",
-      url: "https://identity.whereismytransport.com/connect/token",
-      form: {
-          client_id: CLIENT_ID,
-          client_secret: CLIENT_SECRET,
-          grant_type: "client_credentials",
-          scope: "transportapi:all"
-      }
-  };
-
-  request(clientOptions, function (error, response, body) {
-
-    const TOKEN = JSON.parse(body).access_token;
-
-    var body = {
-      geometry: {
-        type: "Multipoint",
-        coordinates: [[18.425059, -33.922138], [18.399267, -33.908676]]
-      }
-    };
-
-    var options = {
-      method: "POST",
-      headers: {
-        "Accept": "application/json",
-        "Content-Type": "application/json",
-        "Authorization": "Bearer " + TOKEN
-      },
-      url: "https://platform.whereismytransport.com/api/journeys?exclude=geometry",
-      body: JSON.stringify(body)
-    };
-
-    request(options, function (error, response, body) {
-      const result = JSON.parse(body)
-      console.log("Directions.length", result.itineraries[2].legs.length);
-
-      // res.json(result.itineraries[2]);
-    });
-  });
-}
-
-// app.get('/wimt', function (req, res) {
-// 	getJourney()
-// })
 
 app.listen(app.get('port'), function() {
 	console.log('running on port', app.get('port'))
