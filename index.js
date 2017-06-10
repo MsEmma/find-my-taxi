1f5144abcbd52f7c06743fbd3dd1ff4f58e18278
Emma Phiri <emma@uber5.com>
2017-06-10 12:52:46 +0200

    send request updated to promises

--- index.js
+++ index.js
@@ -291,20 +291,28 @@ function sendLocation(sender) {
 }
 
 function sendRequest(sender, messageData) {
-	request({
-		url: 'https://graph.facebook.com/v2.6/me/messages',
-		qs: {access_token: fbToken},
-		method: 'POST',
-		json: {
-			recipient: {id:sender},
-			message: messageData,
-		}
-	}, function(error, response, body) {
-		if (error) {
-			console.log('Error sending messages: ', error)
-		} else if (response.body.error) {
-			console.log('Error: ', response.body.error)
-		}
+	return new Promise((resolve, reject) => {
+		request({
+			url: 'https://graph.facebook.com/v2.6/me/messages',
+			qs: {access_token: fbToken},
+			method: 'POST',
+			json: {
+				recipient: {id:sender},
+				message: messageData,
+			}
+		}, (error, response, body) =>
+		{
+			if (error) {
+				console.log('Error sending messages: ', response.error)
+				return reject(response.error)
+			} else if (response.body.error) {
+				console.log('Response body Error: ', response.body.error)
+				return reject(response.body.error)
+			}
+
+			console.log("Message sent successfully to " + sender);
+			return resolve(response);
+		})
 	})
 }
 
