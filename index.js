951e96ac456eae209d16b0302e826b51c6db422b
Emma <emma@uber5.com>
2017-08-13 14:56:32 +0200

    Refactoring code

--- index.js
+++ index.js
@@ -9,7 +9,10 @@ const app = express()
 
 // const getStops = require('./get-stops')
 const journeyDetails = require('./get-journeys').journeyDetails
-const sendRequest = require('./send-request')
+const sendTextMessage = require('./send-data').textMessage
+const sendJourneySummary = require('./send-data').journeySummary
+const sendLocation = require('./send-data').location
+
 const stops = require('./stops')
 
 app.set('port', (process.env.PORT || 5000))
@@ -27,7 +30,8 @@ app.get('/', function (req, res) {
 
 // for facebook verification
 app.get('/webhook/', function (req, res) {
-	if (req.query['hub.verify_token'] === process.env.VERIFICATION_TOKEN) {
+	// if (req.query['hub.verify_token'] === process.env.VERIFICATION_TOKEN) {
+	if (req.query['hub.verify_token'] === "Iphitaksi") {
 		res.send(req.query['hub.challenge'])
 	} else {
 		res.send('Error, wrong token')
@@ -36,35 +40,30 @@ app.get('/webhook/', function (req, res) {
 
 // to post data
 app.post('/webhook/', function (req, res) {
-
 	const myID = 300416860375397
-	const messaging_events = req.body.entry[0].messaging
-
-	messaging_events.map(event => {
-		const sender = event.sender.id
-
-		if (event.message && event.message.attachments && event.message.attachments.length > 0 && sender != myID) {
-
-			const attachment = event.message.attachments[0]
-			if (attachment.type === 'location') {
-				const loc = attachment.payload.coordinates
-				const dest = getSenderDest(sender)
-
-				displayJourney(sender, loc, dest)
+	const data = req.body.entry[0]
+	
+	if (data.messaging) {
+		const messaging_events = data.messaging
+		messaging_events.map(event => {
+			const sender = event.sender.id
+			
+			if (event.message && event.message.attachments && event.message.attachments.length > 0 && sender != myID) {
+				const attachment = event.message.attachments[0]
+				if (attachment.type === 'location') {
+					const loc = attachment.payload.coordinates
+					const dest = getSenderDest(sender)
+					displayJourney(sender, loc, dest)
+				}
+			} else if (event.postback && event.postback.payload && sender != myID) {
+				const text = JSON.stringify(event.postback)
+				decideMessage(sender, text)
+			} else if (event.message && event.message.text && sender != myID) {
+				const text = event.message.text
+				decideMessage(sender, text)
 			}
-      		
-		} else if (event.postback && event.postback.payload && sender != myID) {
-
-			const text= JSON.stringify(event.postback)
-			decideMessage(sender, text)
-
-		} else if (event.message && event.message.text && sender != myID) {
-
-			const text = event.message.text
-			decideMessage(sender, text)
-
-		}
-	})
+		})
+	}
 	res.sendStatus(200)
 })
 
@@ -107,7 +106,7 @@ function decideMessage(sender, textInput) {
 
 		const checkMode = l => {
 			if(l.mode === "Walking"){
-				sendTextMessage(sender, `Walk ${(l.distance/1000).toFixed(2)} km for ${l.duration} minutes to the taxi stop then`)
+				sendTextMessage(sender, `Walk ${(l.distance/1000).toFixed(2)} km for ${l.duration} minutes`)
 			} else {
 				sendTextMessage(sender,
 				`Take a minibus taxi from ${l.route}, travel for ${(l.distance/1000).toFixed(2)} km in approx ${l.duration} minutes and trip cost is R${l.fare}`)
@@ -160,27 +159,6 @@ function decideMessage(sender, textInput) {
 	}
 }
 
-function sendTextMessage(sender, text) {
-	let messageData = { text:text }
-	sendRequest(sender, messageData)
-}
-
-function sendGenericMessage(sender, messageData) {
-	sendRequest(sender, messageData)
-}
-
-function sendLocation(sender) {
-	let messageData = {
-    "text":"Please share your location:",
-    "quick_replies":[
-      {
-        "content_type":"location"
-      }
-    ]
-  }
-	sendRequest(sender, messageData)
-}
-
 const senderJourney = {}
 
 function storeSenderJourney(senderId, journey) {
@@ -227,62 +205,7 @@ function displayJourney(sender, loc, dest) {
 			}
 		})
 
-		let messageData = {
-
-			"attachment": {
-				"type": "template",
-				"payload": {
-				"template_type": "list",
-				"elements": [
-					{
-						"title": "There are 3 possible routes to your destination",
-						"image_url": "https://fb-s-d-a.akamaihd.net/h-ak-fbx/v/t1.0-9/17103294_300759917007758_1443368003349594057_n.jpg?oh=e67a96ec7acb9ff0e01fc5612402f3eb&oe=5A08F2F2&__gda__=1509960767_b79aca0c3386cbb087cd592f91845532",
-						"subtitle": "Below are the summary details for all routes",
-					},
-					{
-						"title": `Route 2: Walk and use ${ summary[0].noOfTaxis } taxis`,
-						"subtitle": `Total distance: ${ summary[0].routeDistance/1000 } km
-									Total duration: ${ summary[0].routeDuration }mins
-									Total fare: R${ summary[1].routeCost }`,
-						"buttons": [
-							{
-								"type":"postback",
-								"title":"Route 1",
-								"payload":"route1"
-							}
-						]
-					},
-					{
-						"title": `Route 2: Walk and use ${ summary[1].noOfTaxis } taxis`,
-						"subtitle": `Total distance: ${ summary[1].routeDistance/1000 } km
-									Total duration: ${ summary[1].routeDuration }mins
-									Total fare: R${ summary[1].routeCost }`,
-						"buttons": [
-							{
-								"type":"postback",
-								"title":"Route 2",
-								"payload":"route2"
-							}
-						]
-					},
-					{
-						"title": `Route 3: Walk and use ${ summary[2].noOfTaxis } taxis`,
-						"subtitle": `Total distance: ${ summary[2].routeDistance/1000 } km
-									Total duration: ${ summary[2].routeDuration }mins
-									Total fare: R${ summary[2].routeCost }`,
-						"buttons": [
-							{
-								"type":"postback",
-								"title":"Route 3",
-								"payload":"route3"
-							}
-						]
-					},
-				]
-				}
-	    	}
-		}
-		sendGenericMessage(sender, messageData)
+		sendJourneySummary(sender, summary)
 	})
 }
 
---
+++ send-data.js
@@ -0,0 +1,109 @@
+const request = require('request')
+
+function textMessage(sender, text) {
+	let messageData = { text: text }
+	sendRequest(sender, messageData)
+}
+
+function journeySummary(sender, summary) {
+	let messageData = {
+
+		"attachment": {
+			"type": "template",
+			"payload": {
+				"template_type": "list",
+				"elements": [
+					{
+						"title": "There are 3 possible routes to your destination",
+						"image_url": "https://fb-s-d-a.akamaihd.net/h-ak-fbx/v/t1.0-9/17103294_300759917007758_1443368003349594057_n.jpg?oh=e67a96ec7acb9ff0e01fc5612402f3eb&oe=5A08F2F2&__gda__=1509960767_b79aca0c3386cbb087cd592f91845532",
+						"subtitle": "Below are the summary details for all routes",
+					},
+					{
+						"title": `Route 2: Walk and use ${summary[0].noOfTaxis} taxis`,
+						"subtitle": `Total distance: ${summary[0].routeDistance / 1000} km
+									Total duration: ${ summary[0].routeDuration}mins
+									Total fare: R${ summary[1].routeCost}`,
+						"buttons": [
+							{
+								"type": "postback",
+								"title": "Route 1",
+								"payload": "route1"
+							}
+						]
+					},
+					{
+						"title": `Route 2: Walk and use ${summary[1].noOfTaxis} taxis`,
+						"subtitle": `Total distance: ${summary[1].routeDistance / 1000} km
+									Total duration: ${ summary[1].routeDuration}mins
+									Total fare: R${ summary[1].routeCost}`,
+						"buttons": [
+							{
+								"type": "postback",
+								"title": "Route 2",
+								"payload": "route2"
+							}
+						]
+					},
+					{
+						"title": `Route 3: Walk and use ${summary[2].noOfTaxis} taxis`,
+						"subtitle": `Total distance: ${summary[2].routeDistance / 1000} km
+									Total duration: ${ summary[2].routeDuration}mins
+									Total fare: R${ summary[2].routeCost}`,
+						"buttons": [
+							{
+								"type": "postback",
+								"title": "Route 3",
+								"payload": "route3"
+							}
+						]
+					},
+				]
+			}
+		}
+	}
+	sendRequest(sender, messageData)
+}
+
+function location(sender) {
+	let messageData = {
+		"text": "Please share your location:",
+		"quick_replies": [
+			{
+				"content_type": "location"
+			}
+		]
+	}
+	sendRequest(sender, messageData)
+}
+
+function sendRequest(sender, messageData) {
+
+	return new Promise((resolve, reject) => {
+		request({
+			url: 'https://graph.facebook.com/v2.6/me/messages',
+			// qs: {access_token: process.env.PAGE_ACCESS_TOKEN},
+			qs: { access_token: "EAAFePO2rmvwBAMQagXwh4uRZAMpNncMPbpnEDfi6euIiweaajflOE2DkMExFcVQYtA59MsfhWaxvfZAKnLHoJXUleZAoLpVGl1DbNe3gdUlnxZAZADxySk7VcwW5dD54q8M1VUlJwmLLHmlL6VlxR6qimjgp5UeHnYZBzstKbtXgZDZD" },
+			method: 'POST',
+			json: {
+				recipient: {id:sender},
+				message: messageData,
+			}
+		}, (error, response, body) =>
+		{
+			if (error) {
+				console.log('Error sending messages: ', response.error)
+				return reject(response.error)
+			} else if (response.body.error) {
+				console.log('Response body Error: ', response.body.error)
+				return reject(response.body.error)
+			}
+
+			console.log("Message sent successfully to " + sender);
+			return resolve(response);
+		})
+	})
+}
+
+exports.textMessage = textMessage
+exports.journeySummary = journeySummary
+exports.location = location
